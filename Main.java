51743edeabe5db021e3e860febebd0933fc4029e
Mymysmart <httran132@student.hbuhsd.edu>
2025-10-21 21:46:14 +0000

    save me

--- Main.java
+++ Main.java
@@ -4,6 +4,29 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    //Problem 1 
+    // for(int i = 1; i <= 25; i += 2)
+    // {
+    //   System.out.print(i + " ");
+    // }
+  
+
+  
+    //Problem 2 
+    int
+    for(int i = 17; i <= 73; i += 1)
+    {
+      if (counter % 10 == 9);
+      {
+         System.out.print(i +" ");
+      }
+      else
+      {
+         System.out.println();
+      }
+
+      // every 10 numbers
+      
+    }
   }
 }
